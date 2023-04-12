6a44811131b022a4a7426118a19b68b7feda4121
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-04-12 15:13:38 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 import { Layout } from './Main/layout';
 import { Sidebar } from './Main/Component/sidebar';
 import { Dashboard } from './Main/WebPage/dashboard/dashboard';
-import {Appointment} from './Main/WebPage/appointment/appointment'
+import { Appointment } from './Main/WebPage/appointment/appointment'
 import { ROUTES } from './utils/Routes';
 import { Profile } from './Main/WebPage/labProfile/profile';
 import { AppointmentDetails } from './Main/WebPage/appointment/appointmentDetails';
@@ -12,7 +12,8 @@ import { AddTeam } from './Main/WebPage/teamMembers/addTeam';
 import { AddTest } from './Main/WebPage/labTests/addTest';
 import { ManageTeam } from './Main/WebPage/teamMembers/manageTeam';
 import { ManageTest } from './Main/WebPage/labTests/manageTest';
-
+import TokenService from './services/tokenService';
+import { Login } from './Main/WebPage/login';
 
 export const SidebarContext = createContext({ sideBar: false, setSideBar: () => { } })
 
@@ -22,9 +23,12 @@ function App() {
   const getSideBarPos = (value) => {
     setsideBar(!value);
   }
+  const { getStorageData, getToken } = TokenService();
+  const token = getToken();
+  const userType = getStorageData();
 
   return (
-    <>
+    <>  {token && userType.type ?
       <SidebarContext.Provider value={{ sideBar: sideBar, setSideBar: setsideBar }}>
         <Sidebar />
         <div className={sideBar ? 'AppFull' : 'App'}>
@@ -32,7 +36,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path={ROUTES.HOMEPAGE} element={<Dashboard />} />
               <Route path={ROUTES.APPOINTMENT} element={<Appointment />} />
-              <Route path={ROUTES.APPOINTMENT_DETAILS} element={<AppointmentDetails/>} />
+              <Route path={ROUTES.APPOINTMENT_DETAILS} element={<AppointmentDetails />} />
               <Route path={ROUTES.PROFILE} element={< Profile />} />
               <Route path={ROUTES.ADD_TEAM} element={< AddTeam />} />
               <Route path={ROUTES.ADD_TEST} element={< AddTest />} />
@@ -42,9 +46,12 @@ function App() {
           </Routes>
         </div>
       </SidebarContext.Provider>
-      
-      {/* <Login /> */}
-
+      :
+      <Routes>
+        <Route path={ROUTES.HOMEPAGE} element={<Login />} />
+        {/* <Route path='/*' element={<Page404 />}></Route> */}
+      </Routes>
+    }
     </>
   );
 }
